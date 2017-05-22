2e220abc2ed0d5efd14e769c71214764a8ebc1df
wojciech777 <wojtek@wojciechbomba95@gmail.com>
2017-05-22 17:28:40 +0200

    task added and frame of the project

---
+++ src/pl/edu/agh/kis/Main.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.kis;
+
+/**
+ * Created by wojtek on 22.05.17.
+ */
+public class Main {
+    //to Twoja flaga mój młody przyjacielu
+    //tutaj stoczysz swój bój o wiele
+
+    public static void main(String[] args){
+        System.out.println("się zaczęło");
+    }
+}
