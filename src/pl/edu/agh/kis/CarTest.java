9f3564c3625423e545e0e2d487533a0276349174
Wojciech Bomba <wojciechbomba1@gmail.com>
2017-05-22 19:06:56 +0200

    more tests and more test :)

--- src/pl/edu/agh/kis/CarTest.java
+++ src/pl/edu/agh/kis/CarTest.java
@@ -39,5 +39,13 @@ public class CarTest {
         assertEquals("yellow",c.getColor());
     }
 
+    @org.junit.Test
+    public void setColor5() throws Exception {
+        Car c = new Car("TKA",2010,2,"green");
+        c.setColor("yellow");
+
+        assertEquals("yellow",c.getColor());
+    }
+
 
 }
